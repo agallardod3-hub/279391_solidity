9e508578e903db29ba3c2083ff348e7370a60d33
agallardod3-hub <agallardod3@upao.edu.pe>
2026-06-06 20:37:59 -0500

    part1_06/06

---
+++ contracts/18.Payable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+contract Teleton {
+    address public owner;  
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier isOwner() {
+       require(msg.sender == owner, "No eres el propietario");
+       _;
+    }
+    function despositar() public payable {
+    }
+    function retirar() public isOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+}
